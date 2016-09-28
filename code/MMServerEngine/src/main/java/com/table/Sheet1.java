127709cdcf1427ac6d13480c91669479326ca1ec
zhen <zhen>
2016-09-28 21:55:09 +0800

    自动生成的策划配表

---
+++ code/MMServerEngine/src/main/java/com/table/Sheet1.java
@@ -0,0 +1,69 @@
+package com.table;
+//Auto Generate File, Do NOT Modify!!!!!!!!!!!!!!!
+public final class Sheet1{
+	public static final Sheet1[] datas={
+		new Sheet1("xiaoming",(int)12.0,(long)1.23,(float)3.21,java.sql.Timestamp.valueOf("2016-7-23 10:10:10"),"xiaoming",(int)-3.31,(long)-7.705,(float)-12.1,java.sql.Timestamp.valueOf("2016-7-23 10:10:10")),
+		new Sheet1("xiaoming",(int)13.0,(long)1.23,(float)3.21,java.sql.Timestamp.valueOf("2016-7-24 10:10:10"),"xiaoming",(int)-3.97666666667,(long)-8.87166666667,(float)-13.7666666667,java.sql.Timestamp.valueOf("2016-7-24 10:10:10")),
+		new Sheet1("xiaoming",(int)14.0,(long)1.23,(float)3.21,java.sql.Timestamp.valueOf("2016-7-25 10:10:10"),"xiaoming",(int)-4.64333333333,(long)-10.0383333333,(float)-15.4333333333,java.sql.Timestamp.valueOf("2016-7-25 10:10:10")),
+		new Sheet1("xiaoming",(int)15.0,(long)1.23,(float)3.21,java.sql.Timestamp.valueOf("2016-7-26 10:10:10"),"xiaoming",(int)-5.31,(long)-11.205,(float)-17.1,java.sql.Timestamp.valueOf("2016-7-26 10:10:10")),
+		new Sheet1("xiaoming",(int)16.0,(long)1.23,(float)3.21,java.sql.Timestamp.valueOf("2016-7-27 10:10:10"),"xiaoming",(int)-5.97666666667,(long)-12.3716666667,(float)-18.7666666667,java.sql.Timestamp.valueOf("2016-7-27 10:10:10")),
+		new Sheet1("xiaoming",(int)17.0,(long)1.23,(float)3.21,java.sql.Timestamp.valueOf("2016-7-28 10:10:10"),"xiaoming",(int)-6.64333333333,(long)-13.5383333333,(float)-20.4333333333,java.sql.Timestamp.valueOf("2016-7-28 10:10:10")),
+		new Sheet1("xiaoming",(int)18.0,(long)1.23,(float)3.21,java.sql.Timestamp.valueOf("2016-7-29 10:10:10"),"xiaoming",(int)-7.31,(long)-14.705,(float)-22.1,java.sql.Timestamp.valueOf("2016-7-29 10:10:10")),
+		new Sheet1("xiaoming",(int)19.0,(long)1.23,(float)3.21,java.sql.Timestamp.valueOf("2016-7-30 10:10:10"),"xiaoming",(int)-7.97666666667,(long)-15.8716666667,(float)-23.7666666667,java.sql.Timestamp.valueOf("2016-7-30 10:10:10")),
+		new Sheet1("xiaoming",(int)20.0,(long)1.23,(float)3.21,java.sql.Timestamp.valueOf("2016-7-31 10:10:10"),"xiaoming",(int)-8.64333333333,(long)-17.0383333333,(float)-25.4333333333,java.sql.Timestamp.valueOf("2016-7-31 10:10:10")),
+		new Sheet1("xiaoming",(int)21.0,(long)1.23,(float)3.21,java.sql.Timestamp.valueOf("2016-8-1 10:10:10"),"xiaoming",(int)-9.31,(long)-18.205,(float)-27.1,java.sql.Timestamp.valueOf("2016-8-1 10:10:10")),
+		new Sheet1("xiaoming",(int)22.0,(long)1.23,(float)3.21,java.sql.Timestamp.valueOf("2016-8-2 10:10:10"),"xiaoming",(int)-9.97666666667,(long)-19.3716666667,(float)-28.7666666667,java.sql.Timestamp.valueOf("2016-8-2 10:10:10")),
+		new Sheet1("xiaoming",(int)23.0,(long)1.23,(float)3.21,java.sql.Timestamp.valueOf("2016-8-3 10:10:10"),"xiaoming",(int)-10.6433333333,(long)-20.5383333333,(float)-30.4333333333,java.sql.Timestamp.valueOf("2016-8-3 10:10:10")),
+		new Sheet1("xiaoming",(int)24.0,(long)1.23,(float)3.21,java.sql.Timestamp.valueOf("2016-8-4 10:10:10"),"xiaoming",(int)-11.31,(long)-21.705,(float)-32.1,java.sql.Timestamp.valueOf("2016-8-4 10:10:10")),
+		new Sheet1("xiaoming",(int)25.0,(long)1.23,(float)3.21,java.sql.Timestamp.valueOf("2016-8-5 10:10:10"),"xiaoming",(int)-11.9766666667,(long)-22.8716666667,(float)-33.7666666667,java.sql.Timestamp.valueOf("2016-8-5 10:10:10")),
+		new Sheet1("xiaoming",(int)26.0,(long)1.23,(float)3.21,java.sql.Timestamp.valueOf("2016-8-6 10:10:10"),"xiaoming",(int)-12.6433333333,(long)-24.0383333333,(float)-35.4333333333,java.sql.Timestamp.valueOf("2016-8-6 10:10:10")),
+		new Sheet1("xiaoming",(int)27.0,(long)1.23,(float)3.21,java.sql.Timestamp.valueOf("2016-8-7 10:10:10"),"xiaoming",(int)-13.31,(long)-25.205,(float)-37.1,java.sql.Timestamp.valueOf("2016-8-7 10:10:10")),
+		new Sheet1("xiaoming",(int)28.0,(long)1.23,(float)3.21,java.sql.Timestamp.valueOf("2016-8-8 10:10:10"),"xiaoming",(int)-13.9766666667,(long)-26.3716666667,(float)-38.7666666667,java.sql.Timestamp.valueOf("2016-8-8 10:10:10")),
+		new Sheet1("xiaoming",(int)29.0,(long)1.23,(float)3.21,java.sql.Timestamp.valueOf("2016-8-9 10:10:10"),"xiaoming",(int)-14.6433333333,(long)-27.5383333333,(float)-40.4333333333,java.sql.Timestamp.valueOf("2016-8-9 10:10:10")),
+		new Sheet1("xiaoming",(int)30.0,(long)1.23,(float)3.21,java.sql.Timestamp.valueOf("2016-8-10 10:10:10"),"xiaoming",(int)-15.31,(long)-28.705,(float)-42.1,java.sql.Timestamp.valueOf("2016-8-10 10:10:10")),
+		new Sheet1("xiaoming",(int)31.0,(long)1.23,(float)3.21,java.sql.Timestamp.valueOf("2016-8-11 10:10:10"),"xiaoming",(int)-15.9766666667,(long)-29.8716666667,(float)-43.7666666667,java.sql.Timestamp.valueOf("2016-8-11 10:10:10"))
+	};
+	private String name;
+	private int haha;
+	private long ddd;
+	private float fda;
+	private java.sql.Timestamp time;
+	private String name2;
+	private int haha3;
+	private long ddd4;
+	private float fda5;
+	private java.sql.Timestamp time6;
+
+	public Sheet1(String name,int haha,long ddd,float fda,java.sql.Timestamp time,String name2,int haha3,long ddd4,float fda5,java.sql.Timestamp time6){
+		this.name=name;
+		this.haha=haha;
+		this.ddd=ddd;
+		this.fda=fda;
+		this.time=time;
+		this.name2=name2;
+		this.haha3=haha3;
+		this.ddd4=ddd4;
+		this.fda5=fda5;
+		this.time6=time6;
+	}
+	public String getName(){return name;}
+	public void setName(String name){this.name=name;}
+	public int getHaha(){return haha;}
+	public void setHaha(int haha){this.haha=haha;}
+	public long getDdd(){return ddd;}
+	public void setDdd(long ddd){this.ddd=ddd;}
+	public float getFda(){return fda;}
+	public void setFda(float fda){this.fda=fda;}
+	public java.sql.Timestamp getTime(){return time;}
+	public void setTime(java.sql.Timestamp time){this.time=time;}
+	public String getName2(){return name2;}
+	public void setName2(String name2){this.name2=name2;}
+	public int getHaha3(){return haha3;}
+	public void setHaha3(int haha3){this.haha3=haha3;}
+	public long getDdd4(){return ddd4;}
+	public void setDdd4(long ddd4){this.ddd4=ddd4;}
+	public float getFda5(){return fda5;}
+	public void setFda5(float fda5){this.fda5=fda5;}
+	public java.sql.Timestamp getTime6(){return time6;}
+	public void setTime6(java.sql.Timestamp time6){this.time6=time6;}
+}
